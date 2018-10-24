1c246a11e0ecc778e404a61c622c28a98fc2f021
somsasha <somsasha111@gmail.com>
2018-10-25 02:35:34 +0300

    button_fix

--- public/index.js
+++ public/index.js
@@ -1,7 +1,6 @@
 function getArticles() {
     let Inquiry = JSON.stringify({ page: 1, sortOrder: "desc", limit: 5 });
-    //document.body.innerHTML = "<button id='next-button'>Next</button>";
-    document.body.innerHTML = "<input type=\"button\" id=\"next-button\" value=\"Нажми меня\" />";
+    document.body.innerHTML = "<input type=\"text\" value=\"1\" /><button type=\"button\" onclick=\"next()\">GO</button>";
     $.post('http://127.0.0.1:3000/api/articles/readall', Inquiry , (data) => {
         data.items.forEach((item) => {
             document.body.innerHTML += (
@@ -15,8 +14,9 @@ function getArticles() {
 }
 
 function getArticlesNext() {
-    let Inquiry = JSON.stringify({ page: 2, sortOrder: "desc", limit: 5 });
-    document.body.innerHTML = "<button id='next-button'>Next</button>";
+    let pagetogo = $("input:text").val();
+    let Inquiry = JSON.stringify({ page: pagetogo, sortOrder: "desc", limit: 5 });
+    document.body.innerHTML = "<input type=\"text\" value=\"" + pagetogo + "\" /><button type=\"button\" onclick=\"next()\">GO</button>";
     $.post('http://127.0.0.1:3000/api/articles/readall', Inquiry , (data) => {
         data.items.forEach((item) => {
             document.body.innerHTML += (
