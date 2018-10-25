70ab775d9f7a3fdd6571991473fdb5c988a288af
somsasha <somsasha111@gmail.com>
2018-10-25 10:10:17 +0300

    comments

--- public/index.js
+++ public/index.js
@@ -1,30 +1,52 @@
 function getArticles() {
-    let Inquiry = JSON.stringify({ page: 1, sortOrder: "desc", limit: 5 });
+    let Inquiry = JSON.stringify({ page: 1, sortField: "id", sortOrder: "asc", limit: 5 });
     document.body.innerHTML = "<input type=\"text\" value=\"1\" /><button type=\"button\" onclick=\"next()\">GO</button>";
     $.post('http://127.0.0.1:3000/api/articles/readall', Inquiry , (data) => {
         data.items.forEach((item) => {
-            document.body.innerHTML += (
+            let comments = "";
+            let article = "";
+            item.comment.forEach((comment) => {
+                comments += (
+                    "<pre style=\"margin-left: 3em\">" +
+                    "<br/>" + comment.date   +
+                    "<br/>" + comment.text   +
+                    "<br/>" + comment.author +
+                    "<br/>" + "</pre>")
+            });
+            article += (
                 "<br/>" + item.date   +
                 "<br/>" + item.title  +
                 "<br/>" + item.text   +
                 "<br/>" + item.author +
-                "<br/>");
+                "<br/>" );
+            document.body.innerHTML += article + comments;
         });
     });
 }
 
 function getArticlesNext() {
     let pagetogo = $("input:text").val();
-    let Inquiry = JSON.stringify({ page: pagetogo, sortOrder: "desc", limit: 5 });
+    let Inquiry = JSON.stringify({ page: pagetogo, sortField: "id", sortOrder: "asc", limit: 5 });
     document.body.innerHTML = "<input type=\"text\" value=\"" + pagetogo + "\" /><button type=\"button\" onclick=\"next()\">GO</button>";
     $.post('http://127.0.0.1:3000/api/articles/readall', Inquiry , (data) => {
         data.items.forEach((item) => {
-            document.body.innerHTML += (
+            let comments = "";
+            let article = "";
+            item.comment.forEach((comment) => {
+                comments += (
+                    "<pre style=\"margin-left: 3em\">" +
+                    "<br/>" + comment.date   +
+                    "<br/>" + comment.text   +
+                    "<br/>" + comment.author +
+                    "<br/>" + "</pre>")
+            });
+            article += (
                 "<br/>" + item.date   +
                 "<br/>" + item.title  +
                 "<br/>" + item.text   +
                 "<br/>" + item.author +
-                "<br/>");
+                "<br/>" );
+            document.body.innerHTML += article + comments;
         });
     });
 }
